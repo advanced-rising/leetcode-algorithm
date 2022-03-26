86bcdc15dc9434e6f12c0354e966e21a12fd4c1c
risingcore <34502254+advanced-rising@users.noreply.github.com>
2022-03-27 00:58:25 +0900

    Time: 112 ms (68.43%), Space: 47.6 MB (6.98%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let pointer = 0
+    for(let i in nums) {
+        if(nums[i] !== 0 ) {
+            [nums[pointer], nums[i]] = [nums[i], nums[pointer]]
+            pointer++
+        }
+    }
+    return nums
+};
