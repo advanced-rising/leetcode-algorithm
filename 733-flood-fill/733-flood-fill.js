1963a6ad06ae5edae007e6e0098bf72c2f4b6122
risingcore <34502254+advanced-rising@users.noreply.github.com>
2022-03-30 14:53:38 +0900

    Time: 72 ms (93.40%), Space: 44.2 MB (66.35%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function (image, sr, sc, newColor) {
+  const curColor = image[sr][sc];
+  if (newColor === curColor) return image;
+
+  function callDFS(img, row, col) {
+    if (
+      row >= img.length ||
+      row < 0 ||
+      col >= img[0].length ||
+      col < 0 ||
+      img[row][col] !== curColor
+    )
+      return;
+    img[row][col] = newColor;
+    callDFS(img, row - 1, col); // up
+    callDFS(img, row + 1, col); // down
+    callDFS(img, row, col + 1); // right
+    callDFS(img, row, col - 1); // left
+    return img
+  }
+  return callDFS(image, sr, sc);
+};
+
